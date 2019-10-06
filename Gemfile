9ca86e597660f0f45a99e37daa453fbeed05c614
Guillermo Kardolus <kardolus@gmail.com>
2019-10-06 19:41:36 -0400

    Proof of concept

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+ruby '2.3.3'
+
+gem 'pry'
+gem 'json'
+
--- install.sh
+++ install.sh
@@ -2,10 +2,14 @@
 set -e
 
 WORKSPACE=~/workspace
+RUBY_VERSION=2.3.3
 
 mkdir -p $WORKSPACE
+mkdir -p $HOME/go
 
-# Brew
+printf "\nSetup XCode\n"
+xcode-select --install
+
 if ! command -v brew > /dev/null 2>&1; then
   printf "\nInstalling Brew\n"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -15,11 +19,42 @@ printf "\nUpdating Brew\n"
 brew update
 brew upgrade
 
-printf "\nInstalling Packages\n"
+printf "\nSetup Ruby\n"
+brew install rbenv
+rbenv install $RUBY_VERSION
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+rbenv local $RUBY_VERSION
+gem install bundle
+
+printf "\nInstalling Bundle Packages\n"
+bundle install
+
+printf "\nInstalling Brew Packages\n"
 brew bundle
 
+printf "\nSetup Git\n"
+source ./aliases/git
+
 printf "\nSetup Aliases\n"
+cp aliases/bash ~/.aliases
 
 printf "\nSetup Bash\n"
+cp bash/bashrc ~/.bashrc
+cp bash/bash_profile ~/.bash_profile
+
+printf "\nSetup Bash-it\n"
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
+
+source ~/.bash_profile
 
 printf "\nSetup VIM\n"
+wget https://www.dropbox.com/s/hj4567h7h78ncip/vim.zip
+unzip vim.zip
+cp -R vim/.vim ~/.vim
+cp vim/*.vim ~/
+vim +'PlugInstall --sync' +qa
+
+# Note from Mark:
+# python was renamed to pynvim
