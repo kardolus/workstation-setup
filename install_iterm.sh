a37769a56c20b45ed3a8d054e463069409a31eed
Guillermo Kardolus <kardolus@gmail.com>
2020-09-21 11:30:07 -0400

    Create install_iterm.sh

---
+++ install_iterm.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+pushd ~/Downloads
+  wget --output-document latest_iterm.zip https://iterm2.com/downloads/stable/latest
+  unzip latest_iterm.zip
+  mv iTerm.app/ /Applications/
+  rm latest_iterm.zip
+popd
